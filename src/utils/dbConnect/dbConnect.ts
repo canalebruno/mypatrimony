f1990d545195793113eea3111441351fe29afe8d
canalebruno <b.canalemedeiros@gmail.com>
2025-01-05 18:27:39 +0000

    dbconnect fix

--- src/utils/dbConnect/dbConnect.ts
+++ src/utils/dbConnect/dbConnect.ts
@@ -2,7 +2,9 @@ import mongoose from "mongoose"
 
 export async function connect() {
     try {
-        mongoose.connect(process.env.MONGODB_URI as string)
+        mongoose.connect(process.env.MONGODB_URI as string,{
+            dbName:"mypatrimony"
+        })
         const connection = mongoose.connection
         return connection
     } catch (error) {
