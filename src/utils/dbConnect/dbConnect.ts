a127128614172b57ea4063c2448fa18904b4603c
canalebruno <b.canalemedeiros@gmail.com>
2025-01-05 16:48:26 +0000

    mongodb uri

--- src/utils/dbConnect/dbConnect.ts
+++ src/utils/dbConnect/dbConnect.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export async function connect() {
     try {
-        mongoose.connect(process.env.MONGO_URL as string)
+        mongoose.connect(process.env.MONGODB_URI as string)
         const connection = mongoose.connection
         return connection
     } catch (error) {
